9c8b50b36b84f594d2883668366bbc3748b9d14b
hasanosman2020 <hasan79.hoa@gmail.com>
2021-06-17 01:33:43 +0100

    inserted functions to make calls to all 3 APIs

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -6,8 +6,12 @@ const geonamesBaseUrl = 'http://api.geonames.org/searchJSON?q='
 const geonamesApiKey = 'hasan79'
 
 //Weatherbit API
-const weatherbitBaseUrl = 'http://api.weatherbit.io/v2.0/current'
+const weatherbitBaseUrl = 'http://api.weatherbit.io/v2.0/current?'
 const weatherbitApiKey = '334b39cd7f05408190e076877f6411f9'
+
+//Pixabay API
+const pixabayBaseUrl = 'https://pixabay.com/api/?key='
+const pixabayApiKey = '22008827 - e069452971dbec7aa6f1cef1a'
 
 /*Event Listener to add function to existing DOM element ('Let's Go!' button with id 'depart_btn') to create an eventwhen the button is clicked */
 document.getElementById('depart_btn').addEventListener('click', performAction)
@@ -22,16 +26,27 @@ export async function performAction (e) {
   )
 
   getGeonamesData(destination_city).then(function (data) {
-    postData('http://localhost:3000/addGeonamesData', {
+    return getWeatherbitData(
+      weatherbitBaseUrl,
+      data.geonames[0].lat,
+      data.geonames[0].lng,
+      weatherbitApiKey
+    )
+  })
+}
+
+/*postData('http://localhost:3000/addGeonamesData', {
       city: destination_city,
       depart: date_depart,
-      return: date_return
+      return: date_return,
+      latitude: data.geonames[0].lat,
+      longitude: data.geonames[0].lng
     }).then(function () {
       updateUI()
     })
   })
 }
-
+*/
 //Async function makes GET request to the Geonames API to obtain Geonames data (1.e. co-ordinates) using fetch() method
 const getGeonamesData = async destination_city => {
   const res = await fetch(
@@ -46,9 +61,43 @@ const getGeonamesData = async destination_city => {
   }
 }
 
+//Async function makes GET request to the Weatherbit API to obtain Weatherbit API data using fetch() method
+const getWeatherbitData = async (
+  weatherbitBaseUrl,
+  lat,
+  lon,
+  weatherbitApiKey
+) => {
+  const res = await fetch(
+    `${weatherbitBaseUrl}&lat=${lat}&lon=${lon}&key=${weatherbitApiKey}`
+  )
+  try {
+    const data = await res.json()
+    //console.log(data)
+    return data
+  } catch (error) {
+    console.log('error', error)
+  }
+}
+
+//Async function makes GET request to the Pixabay  API to obtain images using fetch() method
+const getPixabay = async (pixabayBaseUrl, pixabayApiKey, destination_city) => {
+  const res = await fetch(
+    `${pixabayBaseUrl}${pixabayApiKey}&q=${destination_city}&image_type=photo`
+  )
+  try {
+    const data = await res.json()
+    console.log(data)
+    return data
+  } catch (error) {
+    console.log('error', error)
+    alert('Cannot find your destination')
+  }
+}
+
 const postData = async () => {
   '/addGeonamesData',
-    (data = { longitude: geonamesData.lon, latitude: geonamesData.lat })
+    (data = { longitude: data.geonames[0].lon, latitude: data.geonames[0].lat })
 }
 
 const postRequest = async () => {
