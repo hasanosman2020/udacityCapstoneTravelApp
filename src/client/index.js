f023200a4875dc0d42cc9c910605ac485144c899
hasanosman2020 <hasan79.hoa@gmail.com>
2021-07-06 01:43:22 +0100

    attempt to debug app

--- src/client/index.js
+++ src/client/index.js
@@ -1,19 +1,18 @@
 console.log('Interesting!')
 
 import { performAction } from './js/app'
-import { getWeatherbitData } from './js/app'
+/*import { getWeatherbitData } from './js/app'
 import { getPixabayData } from './js/app'
 import { postData } from './js/app'
-import { getTimeRemaining } from './js/app'
+import { updateUI } from './js/app'
+/*import { getTimeRemaining } from './js/app'*/
 
 import './styles/style.scss'
 import './styles/header.scss'
 import './styles/footer.scss'
 
-export {
-  performAction,
-  getWeatherbitData,
-  postData,
-  getPixabayData,
-  getTimeRemaining
-}
+export { performAction }
+/*export { getWeatherbitData } from './js/app'
+export { getPixabayData } from './js/app'
+export { postData } from './js/app'
+export { updateUI } from './js/app'*/
--- src/client/js/app.js
+++ src/client/js/app.js
@@ -7,7 +7,7 @@ const geonamesBaseUrl = 'http://api.geonames.org/searchJSON?q='
 const geonamesApiKey = 'hasan79'
 
 //Weatherbit API
-let weatherbitBaseUrl = ' '
+var weatherbitBaseUrl = ' '
 const weatherbitCurrent = 'http://api.weatherbit.io/v2.0/current?'
 const weatherbitForecast = 'http://api.weatherbit.io/v2.0/forecast/daily?'
 const weatherbitApiKey = '334b39cd7f05408190e076877f6411f9'
@@ -19,14 +19,14 @@ const pixabayApiKey = '22008827-e069452971dbec7aa6f1cef1a'
 /*Event Listener to add function to existing DOM element ('Let's Go!' button with id 'depart_btn') to create an eventwhen the button is clicked */
 document.getElementById('depart_btn').addEventListener('click', performAction)
 
-function performAction (e) {
+export async function performAction (e) {
   e.preventDefault()
   const destination_city = document.getElementById('destination_city').value
-  const date_depart = document.getElementById('date_depart').value
+  const dateDepart = document.getElementById('dateDepart').value
   const date_return = document.getElementById('date_return').value
-  const timeRemaining = getTimeRemaining(date_depart)
-  const tripLength = getTripLength(date_depart, date_return)
-  if (timeRemaining <= 7) {
+  const daysTillDepart = getDaysTillDepart(dateDepart)
+  /*const travelDuration = getTravelDuration(date_depart, date_return)*/
+  if (daysTillDepart <= 7) {
     weatherbitBaseUrl = weatherbitCurrent
   } else {
     weatherbitBaseUrl = weatherbitForecast
@@ -45,9 +45,8 @@ function performAction (e) {
       return postData('/', {
         data: data.data,
         destination: destination_city,
-        departure: date_depart,
-        return: date_return,
-        days: timeRemaining
+        departure: dateDepart,
+        return: date_return
       })
     })
     .then(function (data) {
@@ -58,25 +57,14 @@ function performAction (e) {
     })
 }
 
-//Function that generates the time remaining till date of departure
-
-//The variable 'total' holds the remaining time until the date of departure. The Date.parse() function convers a time string into a value in milliseconds which allos us to subtract two times from each other and get the amount of time in between.
-function getTimeRemaining (date_depart) {
-  const total = Date.parse(date_depart) - Date.parse(new Date())
-
-  //convert the miiliseconds to days
-  const days = Math.floor(total / (1000 * 60 * 60 * 24))
-  console.log(days)
+//Function that generates the countdown to date of departure
+function getDaysTillDepart (dateDepart) {
+  const now = new Date()
+  const nowSeconds = Date.parse(now)
+  const dateDepartSeconds = Date.parse(dateDepart)
+  const difference = Math.abs(dateDepartSeconds - nowSeconds)
+  const days = Math.ceil(difference / (1000 * 60 * 60 * 240))
   return days
-}
-
-//Function that generates the number of days of the trip
-function getTripLength (date_depart, date_return) {
-  const triplength_depart = Date.parse(date_depart)
-  const triplength_return = Date.parse(date_return)
-  const diff = Math.abs(triplength_depart - triplength_return)
-  const tripLength = Math.ceil(diff / (1000 * 60 * 60 * 24))
-  return tripLength
 }
 
 //Async function makes GET request to the Geonames API to obtain Geonames data (i.e. co-ordinates) using fetch() method
@@ -95,7 +83,7 @@ const getGeonamesData = async destination_city => {
 }
 
 //Async function makes GET request to the Weatherbit API to obtain Weatherbit API data using fetch() method
-const getWeatherbitData = async (weatherbitBaseUrl, lat, lng) => {
+export const getWeatherbitData = async (weatherbitBaseUrl, lat, lng) => {
   const res = await fetch(
     `${weatherbitBaseUrl}&lat=${lat}&lon=${lng}&key=334b39cd7f05408190e076877f6411f9`
   )
@@ -110,7 +98,7 @@ const getWeatherbitData = async (weatherbitBaseUrl, lat, lng) => {
 }
 
 //Async function makes GET request to the Pixabay  API to obtain images using fetch() method
-const getPixabayData = async (
+export const getPixabayData = async (
   pixabayBaseUrl,
   pixabayApiKey,
   destination_city
@@ -129,8 +117,9 @@ const getPixabayData = async (
   }
 }
 
-//Async function makes a POST request to add the API data to the app using fetch() method
-const postData = async (url = ' ', data = {}) => {
+//Function to POST data
+export const postData = async (url = ' ', data = {}) => {
+  console.log(`Data is ${data}`)
   const res = await fetch('http://localhost:3000', {
     method: 'POST',
     credentials: 'same-origin',
@@ -152,29 +141,18 @@ const updateUI = async imageURL => {
   const req = await fetch('http://localhost:3000/data')
   try {
     const allData = await req.json()
-    console.log(allData)
     document.getElementById('picture').src = imageURL
     document.getElementById('picture').alt = allData.destination
     document.getElementById(
       'location'
-    ).innerHTML = `Your trip is to ${allData.destination}`
-    document.getElementById(
-      'duration'
-    ).innerHTML = `Your trip is ${allData.tripLength} days long.`
+    ).innerHTML = `Your trip to ${allData.destination} is ${allData.daysTillDepart} days away!`
+
     //if trip is less than 4 days away, display the current weather
-    if (allData.days <= 4) {
+    if (allData.daysTillDepart <= 4) {
       document.getElementById('weather').innerHTML =
         'Current weather: ' + allData.data[0].temp + ' °C '
     }
   } catch (error) {
     console.log('error', error)
   }
-}
-
-export {
-  performAction,
-  getTimeRemaining,
-  getWeatherbitData,
-  postData,
-  getPixabayData
 }
